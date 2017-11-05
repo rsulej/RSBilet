e0011adde1e401340d7a74da9ccddaf4533fca03
RaSulej <rsulej@gmail.com>
2017-11-05 21:30:52 +0100

    Nowy projekt SpringBoot

---
+++ src/main/java/com/rs/bilet/RSBilet/RsBiletApplication.java
@@ -0,0 +1,12 @@
+package com.rs.bilet.RSBilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsBiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsBiletApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rs/bilet/RSBilet/RsBiletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rs.bilet.RSBilet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsBiletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
